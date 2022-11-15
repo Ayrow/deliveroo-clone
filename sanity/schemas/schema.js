a88b464fe050474d296f12285db759442984dc2c
Ayrow <aymeric.pilaert@hotmail.fr>
2022-11-15 10:14:28 +0100

    added featured in sanity schema

--- sanity/schemas/schema.js
+++ sanity/schemas/schema.js
@@ -4,15 +4,10 @@ import createSchema from 'part:@sanity/base/schema-creator';
 import schemaTypes from 'all:part:@sanity/base/schema-type';
 
 // We import object and document schemas
-import blockContent from './blockContent';
 import category from './category';
 import restaurant from './restaurant';
-import vendor from './vendor';
-import productVariant from './productVariant';
-
-import localeString from './locale/String';
-import localeText from './locale/Text';
-import localeBlockContent from './locale/BlockContent';
+import dish from './dish';
+import featured from './featured';
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -20,18 +15,5 @@ export default createSchema({
   name: 'default',
   // Then proceed to concatenate our document type
   // to the ones provided by any plugins that are installed
-  types: schemaTypes.concat([
-    // The following are document types which will appear
-    // in the studio.
-    restaurant,
-    vendor,
-    category,
-    // When added to this list, object types can be used as
-    // { type: 'typename' } in other document schemas
-    blockContent,
-    localeText,
-    localeBlockContent,
-    localeString,
-    productVariant,
-  ]),
+  types: schemaTypes.concat([restaurant, category, dish, featured]),
 });
