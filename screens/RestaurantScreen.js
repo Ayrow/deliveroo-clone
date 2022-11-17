33401af97432e09e3b23824e4d3531f9bea73fb8
Ayrow <aymeric.pilaert@hotmail.fr>
2022-11-17 11:47:16 +0100

    added more design to dish row

--- screens/RestaurantScreen.js
+++ screens/RestaurantScreen.js
@@ -8,6 +8,7 @@ import {
   QuestionMarkCircleIcon,
   StarIcon,
 } from 'react-native-heroicons/solid';
+import DishRow from '../components/DishRow';
 const RestaurantScreen = () => {
   const navigation = useNavigation();
 
@@ -82,6 +83,17 @@ const RestaurantScreen = () => {
 
         <View>
           <Text className='px-4 pt-6 mb-3 font-bold text-xl'>Menu</Text>
+
+          {dishes.map((dish) => {
+            <DishRow
+              key={dish._id}
+              id={dish._id}
+              name={dish.name}
+              description={dish.short_description}
+              price={dish.price}
+              image={dish.image}
+            />;
+          })}
         </View>
       </View>
     </ScrollView>
