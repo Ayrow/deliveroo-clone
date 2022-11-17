9f5f6043b7b2183f2096097448d91c4678736469
Ayrow <aymeric.pilaert@hotmail.fr>
2022-11-17 10:13:43 +0100

    fetching categories

--- components/Categories.js
+++ components/Categories.js
@@ -1,23 +1,35 @@
+import { useState } from 'react';
 import { View, Text, ScrollView } from 'react-native';
 import CategoryCard from './CategoryCard';
+import sanityClient, { urlFor } from '../sanity';
+
 const Categories = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    sanityClient
+      .fetch(
+        `
+    *[_type == 'category']
+    `
+      )
+      .then((data) => {
+        setCategories(data);
+      });
+  }, []);
+
   return (
     <ScrollView
       contentContainerStyle={{ paddingHorizontal: 15, paddingTop: 10 }}
       horizontal
       showsHorizontalScrollIndicator={false}>
-      <CategoryCard
-        imgUrl='https://links.papareact.com/gn7'
-        title='testing 1'
-      />
-      <CategoryCard
-        imgUrl='https://links.papareact.com/gn7'
-        title='testing 2'
-      />
-      <CategoryCard
-        imgUrl='https://links.papareact.com/gn7'
-        title='testing 3'
-      />
+      {categories.map((category) => {
+        <CategoryCard
+          key={category._id}
+          imgUrl={urlFor(category.image).width(200).url()}
+          title={category.name}
+        />;
+      })}
     </ScrollView>
   );
 };
