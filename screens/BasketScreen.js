06905f6bbf48efdacae803a7e98a717d8407c45d
Ayrow <aymeric.pilaert@hotmail.fr>
2022-11-18 12:03:54 +0100

    setup restaurant slice

--- screens/BasketScreen.js
+++ screens/BasketScreen.js
@@ -1,5 +1,7 @@
+import { useNavigation } from '@react-navigation/native';
 import { View, Text } from 'react-native';
 const BasketScreen = () => {
+  const navigation = useNavigation();
   return (
     <View>
       <Text>BasketScreen</Text>
